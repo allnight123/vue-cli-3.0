2b9334fb6decb6cfdba624349185e90155a1eb20
Hecle <allnight123@naver.com>
2022-01-31 01:09:47 +0900

    각 파일마다 컴포넌트 명을 확인 하기 위해 내용 추가함

--- src/components/UserDetail.vue
+++ src/components/UserDetail.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="red lighten-3 pa-3">
+    <h5>UserDatail.vue</h5>
     <h3>자세한 회원 정보를 확인합니다.</h3>
     <p>{{ name }}</p>
   </div>
--- src/components/UserEdit.vue
+++ src/components/UserEdit.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="yellow lighten-3 pa-3">
+    <h5>UserEdit.vue</h5>
     <h3>회원 정보를 수정할 수 있습니다.</h3>
     <p>수정사항</p>
   </div>
