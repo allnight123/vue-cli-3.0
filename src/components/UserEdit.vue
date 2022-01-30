624ec5eb56f031673c2140ab3a9ba36ff9eabf01
Hecle <allnight123@naver.com>
2022-01-31 04:11:54 +0900

    UserEdit 파엘에서는 import { eventBus } from '../main'
    methods:에서
    
    eventBus.$emit("userWasEdited" , new Date())
    추가 되었다.

--- src/components/UserEdit.vue
+++ src/components/UserEdit.vue
@@ -25,24 +25,26 @@
 </template>
 
 <script>
-  export default {
-    props: [ 'name','address', 'phone', 'hasDog'],
-    data(){
-      return{
-        user: {}
-      }
-    },
-    created(){
-      this.user.name = this.name
-      this.user.address = this.address
-      this.user.phone = this.phone
-      this.user.hasDog = this.hasDog
-    },
-    methods:{
-      changeUser(){
-        // console.log(this.user)
-        this.$emit("inputchild" , this.user)
-      }
+import { eventBus } from '../main'
+
+export default {
+  props: [ 'name','address', 'phone', 'hasDog'],
+  data(){
+    return{
+      user: {}
     }
-  }   
+  },
+  created(){
+    this.user.name = this.name
+    this.user.address = this.address
+    this.user.phone = this.phone
+    this.user.hasDog = this.hasDog
+  },
+  methods:{
+    changeUser(){
+      this.$emit("inputchild" , this.user)
+      eventBus.$emit("userWasEdited" , new Date())
+    }
+  }
+}   
 </script>
