4ebd0cc979b782ac075ec56e797b25fc1d6e3512
Hecle <allnight123@naver.com>
2022-01-31 02:40:14 +0900

    프롭스에서 전달 받은 값을 직접 바인딩하면 안되기 때문에 User 객체 생성후 User 객체 안에 재할당 하여 값을 바인딩 해주었다.

--- src/components/UserEdit.vue
+++ src/components/UserEdit.vue
@@ -3,8 +3,26 @@
     <h5>UserEdit.vue</h5>
     <h3>회원 정보를 수정할 수 있습니다.</h3>
     <p>수정사항</p>
+    <v-text-field
+      label="Regular"
+      v-model="user.name"
+    ></v-text-field>
   </div>
 </template>
 
 <script>
+  export default {
+    props: [ 'name','address', 'phone', 'hasDog'],
+    data(){
+      return{
+        user: {}
+      }
+    },
+    created(){
+      this.user.name = this.name
+      this.user.address = this.address
+      this.user.phone = this.phone
+      this.user.hasDog = this.hasDog
+    }
+  }   
 </script>
