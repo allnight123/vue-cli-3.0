9efc21c553e7a625c0e1ce4829ca27b1b5448c01
Hecle <allnight123@naver.com>
2022-01-31 01:07:03 +0900

    UserDetail 파일에서 데이터 바인딩을 추가

--- src/components/UserDetail.vue
+++ src/components/UserDetail.vue
@@ -1,6 +1,16 @@
 <template>
   <div class="red lighten-3 pa-3">
     <h3>자세한 회원 정보를 확인합니다.</h3>
-    <p>상세사항</p>
+    <p>{{ name }}</p>
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      name:'뷰 제이에스'
+    }
+  }
+}
+</script>
