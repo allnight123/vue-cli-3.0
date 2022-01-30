7df478f26987badef69333a07d11b09362223998
Hecle <allnight123@naver.com>
2022-01-31 01:35:47 +0900

    Props 속성을 변경하여 자세히 표기한다.

--- src/components/UserDetail.vue
+++ src/components/UserDetail.vue
@@ -2,15 +2,27 @@
   <div class="red lighten-3 pa-3">
     <h5>UserDatail.vue</h5>
     <h3>자세한 회원 정보를 확인합니다.</h3>
-    <p>{{ nameOfChild }}</p>
+    <p>{{ nameOfChild && '님' && sayHello}} </p>
   </div>
 </template>
 
 <script>
 export default {
-  props: ['nameOfChild'],
+  props: {
+    nameOfChild: {
+     type: String,
+     required: false,
+     default:'------'
+    }
+  },
+  computed:{
+    sayHello() {
+      return this.nameOfChild + '안녕하세요'
+    }
+  },
   data() {
     return {
+      age: 30
       // name:'뷰 제이에스'
     }
   }
