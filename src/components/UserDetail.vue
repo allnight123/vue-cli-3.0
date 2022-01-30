6f66cd5b875e1159916a7a585ef1336d9204f0e8
Hecle <allnight123@naver.com>
2022-01-31 02:37:33 +0900

    지금 까지 했던 이름 변경 은 그만하고, 좀더 복잡한내용으로 바꿔줌 4가지 내용이 들어온것을 부모에게 보내고, 부모는 받은것을 다른 자식 컴포넌트에게 데이터를 전달하는 예제임

--- src/components/UserDetail.vue
+++ src/components/UserDetail.vue
@@ -2,34 +2,35 @@
   <div class="red lighten-3 pa-3">
     <h5>UserDatail.vue</h5>
     <h3>자세한 회원 정보를 확인합니다.</h3>
-    <p>{{ nameOfChild && '님' && sayHello}} </p>
-    <v-btn @click="switchName()">이름변경</v-btn>    
+    <p>상세사항</p>
+    <v-list dense>
+      <v-list-tile>
+        <v-list-tile-content>이름:</v-list-tile-content>
+        <v-list-tile-content class="align-end">{{ name }}</v-list-tile-content>
+      </v-list-tile>
+      <v-list-tile>
+        <v-list-tile-content>주소:</v-list-tile-content>
+        <v-list-tile-content class="align-end">{{ address }}</v-list-tile-content>
+      </v-list-tile>
+      <v-list-tile>
+        <v-list-tile-content>전화번호:</v-list-tile-content>
+        <v-list-tile-content class="align-end">{{ phone }}</v-list-tile-content>
+      </v-list-tile>
+      <v-list-tile>
+        <v-list-tile-content>반려견유뮤:</v-list-tile-content>
+        <v-list-tile-content class="align-end">{{ hasDogKr }}</v-list-tile-content>
+      </v-list-tile>
+    </v-list>
+    <!-- <v-btn @click="switchName()">이름변경</v-btn>     -->
   </div>
 </template>
 
 <script>
 export default {
-  props: {
-    nameOfChild: {
-     type: String,
-     required: false,
-     default:'------'
-    }
-  },
+  props: [ 'name','address', 'phone', 'hasDog'],
   computed:{
-    sayHello() {
-      return this.nameOfChild + '안녕하세요'
-    }
-  },
-  methods:{
-    switchName(){
-      this.nameOfChild = '컴퓨터'
-    }
-  },
-  data() {
-    return {
-      age: 30
-      // name:'뷰 제이에스'
+    hasDogKr() {
+      return this.hasDog === true ? '있음' : '없음'
     }
   }
 }
