7540938c85903fcfdd8e8080fe222d0c5526d31c
Hecle <allnight123@naver.com>
2022-02-02 02:58:56 +0900

    views/home.vue  파일을 임포트 를 추가로 해보았다.

--- src/Home.vue
+++ src/Home.vue
@@ -3,11 +3,17 @@
         <AppStatus></AppStatus> home.vue
         <h1>{{ homeTitle }} </h1>
         <AppStatus></AppStatus> home.vue
+        <viewsHome></viewsHome>
     </div>
 </template>
 
 <script>
+import viewsHome from './views/Home.vue'
+
 export default {
+    components: {
+        viewsHome
+    },
     data() {
         return {
             homeTitle: "홈 입니다."
