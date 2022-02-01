a56b0d5b6554e3d32e9f339faf95a6c0dfc79ba0
Hecle <allnight123@naver.com>
2022-02-02 03:32:36 +0900

    UserDetail.vue 파일수정
    부모컴포넌트에서 보내줄 데이터를 받을 준비를 한다.
    nameOfChild 로 받기 위해서는
    props: 에 선언을 먼저 한다.

--- src/components/UserDetail.vue
+++ src/components/UserDetail.vue
@@ -2,5 +2,18 @@
   <div class="red lighten-3 pa-3">
     <h3>자세한 회원 정보를 확인합니다.</h3>
     <p>상세사항</p>
+    <p>{{ nameOfchild }}</p>
+    
   </div>
 </template>
+
+<script>
+export default {
+  props: [ 'nameOfchild' ],           // 값을 받는쪽 에다가 선언해줘야 한다.
+  data() {
+    return {
+      // nameOfchild 
+    }
+  },
+}
+</script>
