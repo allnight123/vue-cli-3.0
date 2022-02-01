04bc7fb4f423ed6c6f5563c41f11e03804e51e29
Hecle <allnight123@naver.com>
2022-02-02 03:29:40 +0900

    User.vue 파일수정 이름을 변수로 바꾸고 changeName 함수를 선언 자식 컴포넌트에 데이터를 nameOfChild 라는 이름으로 전달해준다.

--- src/components/User.vue
+++ src/components/User.vue
@@ -1,11 +1,14 @@
 <template>
   <div class="blue lighten-3 pa-3">
     <h1>User 컴포넌트</h1>
-    <p>이름: 뷰제이에스</p>
+    <p>이름: {{ name }}</p>
+    <v-btn @click="changeName()" >이름변경</v-btn>
     <hr>
     <v-layout row wrap>
       <v-flex xs12 sm6>
-        <UserDetail></UserDetail>
+        <UserDetail
+          :nameOfchild = name
+          ></UserDetail>
       </v-flex>
       <v-flex xs12 sm6>
         <UserEdit></UserEdit>
@@ -22,6 +25,16 @@ export default {
   components: {
     UserDetail,
     UserEdit
-  }
+  },
+  data() {
+    return {
+      name: '뷰 제이에스'
+    }
+  },
+  methods: {
+    changeName() {
+      this.name = 'hoza'
+    }
+  },
 }
 </script>
