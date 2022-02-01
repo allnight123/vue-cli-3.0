72aaac08396dfb00640b9021ddeeba0ea8f70912
Hecle <allnight123@naver.com>
2022-02-02 03:00:40 +0900

    main.js 에 주석으로 설명 추가 컴포넌트를 등록하기 위한 예제
    main.js 에 등록된 컴포넌트는 아무대서나 사용 가능하다.

--- src/main.js
+++ src/main.js
@@ -4,9 +4,10 @@ import './plugins/vuetify'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import StatusComponents from './Status.vue'
+import StatusComponents from './Status.vue'     // 새로운 컴포넌트 등록시 
 
-Vue.component('AppStatus', StatusComponents)
+Vue.component('AppStatus', StatusComponents)    // 새로운 컴포넌트 등록시 
+                                                // 이렇게 등록 하면 전역에서 사용가능하다.
 
 Vue.config.productionTip = false
 
