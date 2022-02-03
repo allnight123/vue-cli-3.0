8d352a1b51fb74a7cbd228962cd0c3b410afc323
Hecle <allnight123@naver.com>
2022-02-03 18:20:53 +0900

    UserDetail.vue 수정
    html
    UI 컴포넌트 추가 하여 데이터 바인딩
    
    script
    props 로 데이터를 전달 받고
    hasDog 함수 추가

--- src/components/UserDetail.vue
+++ src/components/UserDetail.vue
@@ -2,18 +2,43 @@
   <div class="red lighten-3 pa-3">
     <h3>자세한 회원 정보를 확인합니다.</h3>
     <p>상세사항</p>
-    <p>{{ nameOfchild }}</p>
-    
+    <v-list>
+      <v-list-tile>
+        <v-list-tile-content>이름 :</v-list-tile-content>
+        <v-list-tile-content class="align-end">
+          {{ name }}
+          </v-list-tile-content>
+      </v-list-tile>
+      <v-list-tile>
+        <v-list-tile-content>주소 :</v-list-tile-content>
+        <v-list-tile-content class="align-end"
+          >{{ address }}
+        </v-list-tile-content>
+      </v-list-tile>
+      <v-list-tile>
+        <v-list-tile-content>전화번호 :</v-list-tile-content>
+        <v-list-tile-content class="align-end"
+          >{{ phone }}
+        </v-list-tile-content>
+      </v-list-tile>
+      <v-list-tile>
+        <v-list-tile-content>반려견유뮤 :</v-list-tile-content>
+        <v-list-tile-content class="align-end"
+          >{{ hasDogKr }}
+        </v-list-tile-content>
+      </v-list-tile>
+    </v-list>
   </div>
 </template>
 
 <script>
 export default {
-  props: [ 'nameOfchild' ],           // 값을 받는쪽 에다가 선언해줘야 한다.
-  data() {
-    return {
-      // nameOfchild 
-    }
+  props: ["name", "address", "phone", "hasDog"], // 값을 받는쪽 에다가 선언해줘야 한다.
+
+  computed: {
+    hasDogKr() {
+      return this.hasDog === true ? "있음" : "없음";
+    },
   },
-}
+};
 </script>
