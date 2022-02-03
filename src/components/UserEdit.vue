45bbe25ac0d423fd88bd834327400b7f246153c2
Hecle <allnight123@naver.com>
2022-02-03 18:25:53 +0900

    UserEdit.vue 수정(내용 추가)
    html
    text-field 추가 하고 데이터 바인딩
    수정완료 버튼 추가
    
    script
    props 로 부모로 부터 받을 데이터 세팅
    user 객체 생성
    created 훅으로 user 객체로 부터 받은 값을 벤수에 재할당
    
    methods
    changeUser함수 추가
    부모부품에 신호전달을 위해서 $emit 사용, user객체 파라메터 전달

--- src/components/UserEdit.vue
+++ src/components/UserEdit.vue
@@ -2,8 +2,38 @@
   <div class="yellow lighten-3 pa-3">
     <h3>회원 정보를 수정할 수 있습니다.</h3>
     <p>수정사항</p>
+    <v-text-field label="이름" placeholder="Placeholder" v-model="user.name">    </v-text-field>
+    <v-text-field label="주소" placeholder="Placeholder" v-model="user.address">    </v-text-field>
+    <v-text-field label="전화번호" placeholder="Placeholder" v-model="user.phone">    </v-text-field>
+    <v-radio-group column v-model="user.hasDog">
+      <v-radio label="반려견 있음" value="true"></v-radio>
+      <v-radio label="반려견 없음" value="false"></v-radio>      
+    </v-radio-group>
+    <v-btn @click="changeUser()">수정완료</v-btn>
   </div>
 </template>
 
 <script>
+export default {
+  props: ["name", "address", "phone", "hasDog"], // 값을 받는쪽 에다가 선언해줘야 한다.
+  data() {
+    return {
+      user: {},
+    };
+  },
+
+  created() {
+    this.user.name = this.name
+    this.user.address = this.address
+    this.user.phone = this.phone
+    this.user.hasDog = this.hasDog
+  },
+
+  methods: {
+    changeUser() {
+      console.log(this.user)
+      this.$emit("child" , this.user)
+    }
+  }
+};
 </script>
