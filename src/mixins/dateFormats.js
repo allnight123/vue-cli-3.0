c3beef2f70fac6b3d759389a741c668e31fde995
Hecle <allnight123@naver.com>
2022-02-06 22:14:44 +0900

    dateFormats.js 내용추가
    mixins 의 우선순위 로딩을 확인 하기위한
    로그 출력
    
      created() {
        console.log('믹스인')
      },

--- src/mixins/dateFormats.js
+++ src/mixins/dateFormats.js
@@ -1,15 +1,24 @@
 export const dateFormat = {
-    methods: {
-        getDateAndTime(date) {
-          if ( date !== null) {
-            let hour = date.getHours()
-            let minutes = date.getMinutes()
-            let fullDate = `${date.getFullYear()} / ${date.getMonth() + 1} / ${date.getDate()}`
-            return `${fullDate} ${hour} : ${minutes}`
-          }
-          else {
-            return null
-          }
-        }
+  created() {
+    console.log('믹스인')
+  },
+  data() {
+    return {
+      mixinData: '나는 믹스인이오!'
+    }
+  },
+
+  methods: {
+    getDateAndTime(date) {
+      if (date !== null) {
+        let hour = date.getHours()
+        let minutes = date.getMinutes()
+        let fullDate = `${date.getFullYear()} / ${date.getMonth() + 1} / ${date.getDate()}`
+        return `${fullDate} ${hour} : ${minutes}`
       }
+      else {
+        return null
+      }
+    }
+  }
 }
